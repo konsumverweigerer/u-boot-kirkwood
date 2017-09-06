lentinj/u-boot

kirkwood: Rename dns325 to dnskw
So we can re-use DNS-325 configuration for the DNS-320 without things getting
confusing, rename all common parts from dns325 to dnskw, and use a config
option to configure DNS-325 specifics.

Signed-off-by: Jamie Lentin <jm@lentin.co.uk>
Cc: prafulla@marvell.com
Cc: albert.u.boot@aribaud.net

board/d-link/dnskw/dnskw.c
@@ -33,15 +33,15 @@
 #include <asm/arch/kirkwood.h>
 #include <asm/arch/mpp.h>
 #include <asm/arch/gpio.h>
-#include "dns325.h"
+#include "dnskw.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int board_early_init_f(void)
 {
 	/* Gpio configuration */
-	kw_config_gpio(DNS325_OE_VAL_LOW, DNS325_OE_VAL_HIGH,
-			DNS325_OE_LOW, DNS325_OE_HIGH);
+	kw_config_gpio(DNSKW_OE_VAL_LOW, DNSKW_OE_VAL_HIGH,
+			DNSKW_OE_LOW, DNSKW_OE_HIGH);
 
 	/* Multi-Purpose Pins Functionality configuration */
 	u32 kwmpp_config[] = {
@@ -99,9 +99,9 @@ int board_early_init_f(void)
 	};
 	kirkwood_mpp_conf(kwmpp_config);
 
-	kw_gpio_set_blink(DNS325_GPIO_LED_POWER , 1);
+	kw_gpio_set_blink(DNSKW_GPIO_LED_POWER , 1);
 
-	kw_gpio_set_value(DNS325_GPIO_SATA0_EN , 1);
+	kw_gpio_set_value(DNSKW_GPIO_SATA0_EN , 1);
 	return 0;
 }
 

konsumverweigerer/u-boot-kirkwood

board/d-link/dnskw/dnskw.c
@@ -17,15 +17,15 @@
 #include <asm/arch/soc.h>
 #include <asm/arch/mpp.h>
 #include <asm/arch/gpio.h>
-#include "dns325.h"
+#include "dnskw.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int board_early_init_f(void)
 {
 	/* Gpio configuration */
-	mvebu_config_gpio(DNS325_OE_VAL_LOW, DNS325_OE_VAL_HIGH,
-			  DNS325_OE_LOW, DNS325_OE_HIGH);
+	mvebu_config_gpio(DNSKW_OE_VAL_LOW, DNSKW_OE_VAL_HIGH,
+			  DNSKW_OE_LOW, DNSKW_OE_HIGH);
 
 	/* Multi-Purpose Pins Functionality configuration */
 	static const u32 kwmpp_config[] = {
@@ -83,9 +83,9 @@ int board_early_init_f(void)
 	};
 	kirkwood_mpp_conf(kwmpp_config, NULL);
 
-	kw_gpio_set_blink(DNS325_GPIO_LED_POWER , 1);
+	kw_gpio_set_blink(DNSKW_GPIO_LED_POWER , 1);
 
-	kw_gpio_set_value(DNS325_GPIO_SATA0_EN , 1);
+	kw_gpio_set_value(DNSKW_GPIO_SATA0_EN , 1);
 	return 0;
 }
 